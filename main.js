1b5fdfa87869e2622b0588d2afdff7e820422ce0
Vince Lavang <vincelavang@gmail.com>
2017-06-09 16:17:04 -0700

    Broke string into an array.#3

---
+++ main.js
@@ -0,0 +1,4 @@
+var $content = document.querySelector('#content')
+var string = '"Back in my heyday, I would kill Michael Jordan one-on-one." - LaVar Ball'
+$content.textContent = string
+var chars = string.split('')
