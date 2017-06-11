2d0303f5c70b2168aeb57a47376803b7abadece6
Vince Lavang <vincelavang@gmail.com>
2017-06-11 13:42:02 -0700

    Created for loop to create span.#3

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 var $content = document.querySelector('#content')
 var string = '"Back in my heyday, I would kill Michael Jordan one-on-one." - LaVar Ball'
-$content.textContent = string
 var chars = string.split('')
+
+for (var i = 0; i < string.length; i++) {
+  $content.innerHTML += '<span>' + string[i] + '</span>'
+}
